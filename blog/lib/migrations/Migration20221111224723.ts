5f8c4d39b16ab9eba265983ae386332112fe8ade
Dale Salter <dalejsalter@outlook.com>
2022-11-20 17:10:00 -0500

    ADDED: Changes

--- blog/lib/migrations/Migration20221111224723.ts
+++ blog/lib/migrations/Migration20221111224723.ts
@@ -1,13 +1,13 @@
-import { Migration } from '@mikro-orm/migrations';
+import { Migration } from '@mikro-orm/migrations'
 
 export class Migration20221111224723 extends Migration {
-
   async up(): Promise<void> {
-    this.addSql('create table "book" ("uuid" varchar(255) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "title" varchar(255) not null, constraint "book_pkey" primary key ("uuid"));');
+    this.addSql(
+      'create table "book" ("uuid" varchar(255) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "title" varchar(255) not null, constraint "book_pkey" primary key ("uuid"));'
+    )
   }
 
   async down(): Promise<void> {
-    this.addSql('drop table if exists "book" cascade;');
+    this.addSql('drop table if exists "book" cascade;')
   }
-
 }
