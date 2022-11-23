5282a91160a7305fcc02b08dd0157f43a5cbe4eb
Dale Salter <dalejsalter@outlook.com>
2022-11-22 23:32:14 -0500

    ADDED: Improvements to otel implementation

--- blog/lib/blog-infrastructure.ts
+++ blog/lib/blog-infrastructure.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway'
-import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda'
+import { Architecture, LayerVersion, Runtime, Tracing } from 'aws-cdk-lib/aws-lambda'
 import { SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2'
 import { ARecord, PublicHostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53'
 import * as secretsManager from 'aws-cdk-lib/aws-secretsmanager'
@@ -53,24 +53,40 @@ export class BlogInfrastructure extends cdk.Stack {
       memorySize: 1024,
       timeout: Duration.seconds(30),
       entry: __dirname + '/blog-infrastructure-lambda.function.ts',
+      tracing: Tracing.ACTIVE,
       environment: {
         STAGE,
         OTEL_EXPORTER_OTLP_ENDPOINT,
         OTEL_EXPORTER_OTLP_HEADERS,
         OTEL_SERVICE_NAME,
+        AWS_LAMBDA_EXEC_WRAPPER: '/opt/otel-handler',
+        OPENTELEMETRY_COLLECTOR_CONFIG_FILE: '/var/task/collector.yaml',
         DATABASE_SECRET_ARN: secret.secretFullArn ?? '',
       },
+      layers: [
+        LayerVersion.fromLayerVersionArn(
+          this,
+          'otel-layer',
+          'arn:aws:lambda:us-east-1:901920570463:layer:aws-otel-nodejs-arm64-ver-1-7-0:2'
+        ),
+      ],
       bundling: {
         preCompilation: true, // Runs TSC before deploying
+        keepNames: true, // This option might do something
+
+        commandHooks: {
+          beforeBundling(inputDir: string, outputDir: string): string[] {
+            return [`cp ${inputDir}/collector.yaml ${outputDir}`]
+          },
+          afterBundling(): string[] {
+            return []
+          },
+          beforeInstall() {
+            return []
+          },
+        },
 
         nodeModules: [
-          // Required for 2 reasons
-          //  - Fixes an issue with '@opentelemetry/sdk-node' -> thriftrw -> bufrw throwing an error on function initialization
-          //  - OTEL does not work if `sdk-node` and `auto-instrumentations-node` are not included
-          '@opentelemetry/api',
-          '@opentelemetry/sdk-node',
-          '@opentelemetry/auto-instrumentations-node',
-
           // Requires these to be installed as node_modules otherwise
           //  auto-instrumentation does not work. Impacts of including = ~700ms - 1s to cold start
           'graphql',
@@ -81,6 +97,11 @@ export class BlogInfrastructure extends cdk.Stack {
         externalModules: [
           // pg imports
           'pg-native',
+
+          // TODO: Support local otel
+          '@opentelemetry/api',
+          '@opentelemetry/sdk-node',
+          '@opentelemetry/auto-instrumentations-node',
 
           // Mikro-orm imports -- is there a better way?
           '@mikro-orm/entity-generator',
