5b4bb40455d1c8d3c9b68779c7c4f25b06271513
Dale Salter <dalejsalter@outlook.com>
2023-01-01 18:25:47 -0500

    FIXED: Sitemap to include about me

--- blog-frontend/pages/sitemap.xml.js
+++ blog-frontend/pages/sitemap.xml.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { request, gql } from 'graphql-request'
 
-const EXTERNAL_DATA_URL = 'https://dalejsalter.com/post'
+// e.g. https://dalejsalter.com
+const ROOT_URL = process.env.NEXT_PUBLIC_VERCEL_URL
 
 const GET_POSTS = gql`
   query {
@@ -20,17 +21,19 @@ const lastModifiedDate = ({ posts }) => {
 const createSitemap = ({ posts }) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
-            <loc>https://dalejsalter.com/</loc>
+            <loc>${ROOT_URL}</loc>
             <changefreq>weekly</changefreq>
             <lastmod>${lastModifiedDate({ posts })}</lastmod>
             <priority>1.00</priority>
         </url>
-
+        <url>
+          <loc>${`${ROOT_URL}/about`}</loc>
+        </url>
         ${posts
           .map((post) => {
             return `
                     <url>
-                        <loc>${`${EXTERNAL_DATA_URL}/${post.id}/${post.urlStub}`}</loc>
+                        <loc>${`${ROOT_URL}/post/${post.id}/${post.urlStub}`}</loc>
                     </url>
                 `
           })
