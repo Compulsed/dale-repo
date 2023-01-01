b3f7c8bf111c42de2faed50564c14cf27d9fc22a
Dale Salter <dalejsalter@outlook.com>
2023-01-01 18:46:18 -0500

    ADDED: Sitemap

---
+++ blog-frontend/next-sitemap.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_VERCEL_URL,
+  generateRobotsTxt: true,
+  transform: async (config, path) => {
+    if (path.startsWith(`${process.env.NEXT_PUBLIC_VERCEL_URL}/admin`)) {
+      return null
+    }
+
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
+}
